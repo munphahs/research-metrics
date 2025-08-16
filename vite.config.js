ea706015f2c5b7d139491c0f0817505e99541bae
PHAHS <phahs.genoffice@mun.ca>
2025-08-16 11:03:48 -0230

    base updated to repo name

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/mun-repo-dashboard/",
-});
+export default {
+  base: '/<research-metrics>/',
+}
